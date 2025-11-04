a5bb378c9e56b1d5cec62a0c406bacde66419f1a
ProM113K <putrad322@gmail.com>
2025-11-04 13:12:01 +0700

    memperbaiki form setelah submit kehadiran

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Terima kasih telah memberikan konfirmasi!");
+    })
+  });
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     const guestList = {
         'A01': 'Alumni SDN Sukamaju 5',
